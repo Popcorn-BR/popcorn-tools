5004d10f8c5f9ef090e86b0fc78a0065967fc552
Denner Rondinely <denner.rondinely@gmail.com>
2020-06-13 02:03:46 -0300

    Update cpfValidate.spec.js

--- tests/cpfValidate.spec.js
+++ tests/cpfValidate.spec.js
@@ -16,22 +16,22 @@ describe('CPF Validate', () => {
 
   describe('CPF Validated', () => {
     it('should call validated true', () => {
-      const cpf = cpfValidate('10668552697');
+      const cpf = cpfValidate('45317828791');
       expect(cpf).to.have.been.eqls(true);
     });
 
     it('should call validated false', () => {
-      const cpf = cpfValidate('10668552698');
+      const cpf = cpfValidate('45317828792');
       expect(cpf).to.have.been.eqls(false);
     });
 
     it('should call validated replace true', () => {
-      const cpf = cpfValidate('106.685.526-97');
+      const cpf = cpfValidate('453.178.287-91');
       expect(cpf).to.have.been.eqls(true);
     });
 
     it('should call validated replace false', () => {
-      const cpf = cpfValidate('106.685.526,97');
+      const cpf = cpfValidate('453.178.287,91');
       expect(cpf).to.have.been.eqls(false);
     });
   });
