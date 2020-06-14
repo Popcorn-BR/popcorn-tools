7e98c39251a74300c8ec072a324ade420a28437d
Denner Rondinely <denner.rondinely@gmail.com>
2020-06-14 13:12:15 -0300

    testes do methodo filter

---
+++ tests/filter.spec.js
@@ -0,0 +1,40 @@
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+
+import filter from '../src/filter';
+
+chai.use(sinonChai);
+
+describe('Filter', () => {
+  const data = [
+    {value: 'Banana'},
+    {value: 'Maçã'},
+    {value: 'Laranja'},
+    {value: 'Morango'},
+    {value: 'Abacaxi'},
+    {value: 'Mamão'},
+  ]
+
+  describe('smoke tests', () => {
+    it('should exist the filter method', () => {
+      expect(filter).to.exist;
+    });
+  });
+
+  describe('Filter', () => {
+    it('should call filter by m', () => {
+      const dataFilter = filter(data, 'mam', 'value');
+      expect(dataFilter).to.have.been.eqls([{ value: "Mamão" }]);
+    });
+
+    it('should call filter not valid value', () => {
+      const dataFilter = filter(data, 'abc', 'value');
+      expect(dataFilter).to.have.been.eqls([]);
+    });
+
+    it('should call filter empty value', () => {
+      const dataFilter = filter(data, '', 'value');
+      expect(dataFilter).to.have.been.eqls(data);
+    });
+  });
+});
