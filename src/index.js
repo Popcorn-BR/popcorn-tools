18e64b53bde5fe9fb1f95d2b636c116968c5d049
Denner Rondinely <denner.rondinely@gmail.com>
2020-06-20 18:35:30 -0300

    Bump versio 0.2.1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import currencyFormat from './currencyFormat';
 import cpfValidate from './cpfValidate';
 import filter from './filter';
 import measureFormat from './measureFormat';
+import mobileCheck from './mobileCheck';
 
 export default {
   currencyFormat,
   cpfValidate,
   filter,
   measureFormat,
+  mobileCheck,
 };
